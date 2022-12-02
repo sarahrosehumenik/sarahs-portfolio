2f7a4b6ac187cb25cb6e2e1c102330ae20d426f6
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-12-02 16:34:05 -0700

    styling for landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,10 +6,12 @@ export default function Landing() {
         <div className="landing-container">
             <div className="spacer"></div>
       <div className="top-section"> 
-      <div className="space-one"><img className="image-one" src="https://i.imgur.com/Tt4EQDN.jpg"></img></div>
+      <div className="space-one"><img className="image-one" src="https://i.imgur.com/RWKOaGk.jpg"/></div>
       <div className="landing-text">
           <h1 className="name">Sarah Rose</h1>
           <p className="title">Full Stack Web Developer</p>
+          <p className="about">I am a Colorado based Software Engineer taking my eye for design and problem solving love to the tech field. 
+          Currently teaching Full Stack as an Insturctor with General Assembly. </p>
          
     </div>
        </div>
@@ -19,7 +21,7 @@ export default function Landing() {
                <p>Email</p>
                <p>links</p>
                </div>
-               <div className="space-three"></div>
+               <div className="space-three"><img className="image-two" src="https://i.imgur.com/quIOZhy.jpg"/></div>
        </div>
        
 
