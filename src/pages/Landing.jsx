f3c65925bc1a6f67e07f933fa4230af1af31900a
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-12-02 16:57:16 -0700

    colors on landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,27 +5,30 @@ export default function Landing() {
     return (
         <div className="landing-container">
             <div className="spacer"></div>
-      <div className="top-section"> 
-      <div className="space-one"><img className="image-one" src="https://i.imgur.com/RWKOaGk.jpg"/></div>
-      <div className="landing-text">
-          <h1 className="name">Sarah Rose</h1>
-          <p className="title">Full Stack Web Developer</p>
-          <p className="about">I am a Colorado based Software Engineer taking my eye for design and problem solving love to the tech field. 
-          Currently teaching Full Stack as an Insturctor with General Assembly. </p>
-         
-    </div>
-       </div>
-       <div className="bottom-section">
-           <div className="space-two"></div>
-           <div className="contact-info">
-               <p>Email</p>
-               <p>links</p>
-               </div>
-               <div className="space-three"><img className="image-two" src="https://i.imgur.com/quIOZhy.jpg"/></div>
-       </div>
-       
+            <div className="top-section">
+                <div className="space-one"><img className="image-one" src="https://i.imgur.com/RWKOaGk.jpg" /></div>
+                <div className="landing-text">
+                    <h1 className="name">Sarah Rose</h1>
+                    <p className="title">Full Stack Web Developer</p>
+                    <p className="about">I am a Colorado based Software Engineer taking my eye for design and problem solving love to the tech field.
+                        Currently teaching Full Stack as an Instructor with General Assembly. </p>
 
- 
-  </div>
+                </div>
+            </div>
+            <div className="bottom-section">
+                <div className="space-two"></div>
+                <div className="contact-info">
+                    <a className="email" href="mailto:sarahrosehumenik@gmail.com">Email: Sarahrosehumenik@gmail.com</a>
+                    <div className="logos-container">
+                        <a href="https://github.com/sarahrosehumenik"><i id="contact-logo" class="fa-brands fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/sarah-rose33/"><i id="contact-logo" class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div className="space-three"><img className="image-two" src="https://i.imgur.com/quIOZhy.jpg" /></div>
+            </div>
+
+
+
+        </div>
     )
 }
