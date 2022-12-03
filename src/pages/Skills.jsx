b80c72d81fd8b3438ea1be63c611225699e0016e
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-12-02 21:02:14 -0700

    Skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,17 +1,40 @@
 import './Skills.css'
-
+import { useState } from "react";
 export default function Skills() {
-    return (
-        <div id="Header" className="header">
-        <div className="headertext-skills">
-        <h1>Skills</h1>
-       
-        <img className="skills" src="https://i.imgur.com/fPM13A4.png"></img>
 
-     
-       
-  </div>
-  </div>
-    )
+const [currentSkill, setCurrentSkill] = useState("My Skills")
 
+const javaScript = () => {
+    setCurrentSkill('JavaScript')
 }
+const html = () => {
+    setCurrentSkill('HTML')
+}
+
+const css = () => {
+    setCurrentSkill('CSS')
+}
+
+
+    return (
+<div className="skills-container">
+    <div className="display"><h1 className="skill-text">{currentSkill}</h1></div>
+    <div className="skills-list">
+        <p onClick={javaScript} className="icon-container"><i id="i" class="fa-brands fa-js"></i></p>
+        <p onClick={html} className="icon-container"><i id="i" class="fa-brands fa-html5"></i></p>
+        <p onClick={css} className="icon-container"><i id="i" class="fa-brands fa-css3-alt"></i></p>
+        <p className="icon-container">d</p>
+        <p className="icon-container">e</p>
+        <p className="icon-container">f</p>
+        <p className="icon-container">g</p>
+        <p className="icon-container">h</p>
+        <p className="icon-container">i</p>
+        <p className="icon-container">j</p>
+        <p className="icon-container">k</p>
+        <p className="icon-container">l</p>
+        <p className="icon-container">m</p>
+        <p className="icon-container">n</p>
+        <p className="icon-container">o</p>
+      </div>
+</div>
+    )}
