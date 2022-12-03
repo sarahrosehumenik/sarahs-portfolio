101efc66837e04f6ceb43a193fe6f08ae03f1efc
Sarah Rose <you@example.com>
2022-12-03 10:25:51 -0700

    skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -14,8 +14,47 @@ const html = () => {
 const css = () => {
     setCurrentSkill('CSS')
 }
+const python = () => {
+    setCurrentSkill("Python")
+}
 
+const django = () => {
+    setCurrentSkill("Django")
+}
+const aws = () => {
+    setCurrentSkill("Amazon Web Service")
+}
+const stripe = () => {
+    setCurrentSkill("Stripe API")
+}
+const node = () => {
+    setCurrentSkill("Node.js")
+}
 
+const databases = () => {
+    setCurrentSkill("MongoDB & PosgreSQL")
+}
+
+const react = () => {
+    setCurrentSkill("React")
+}
+const github = () => {
+    setCurrentSkill("GitHub")
+}
+
+const google = () => {
+    setCurrentSkill("Google Oauth")
+}
+
+const mongoose = () => {
+    setCurrentSkill("Mongoose")
+}
+const vsCode = () => {
+    setCurrentSkill("Visual Studio Code")
+}
+const postman = () => {
+    setCurrentSkill("Postman")
+}
     return (
 <div className="skills-container">
     <div className="skills-description">
@@ -28,18 +67,18 @@ const css = () => {
         <p onClick={javaScript} className="icon-container"><i id="i" class="fa-brands fa-js"></i></p>
         <p onClick={html} className="icon-container"><i id="i" class="fa-brands fa-html5"></i></p>
         <p onClick={css} className="icon-container"><i id="i" class="fa-brands fa-css3-alt"></i></p>
-        <p className="icon-container">d</p>
-        <p className="icon-container">e</p>
-        <p className="icon-container">f</p>
-        <p className="icon-container">g</p>
-        <p className="icon-container">h</p>
-        <p className="icon-container">i</p>
-        <p className="icon-container">j</p>
-        <p className="icon-container">k</p>
-        <p className="icon-container">l</p>
-        <p className="icon-container">m</p>
-        <p className="icon-container">n</p>
-        <p className="icon-container">o</p>
+        <p onClick={python} className="icon-container"><i id="i" class="fa-brands fa-python"></i></p>
+        <p onClick={django}  className="icon-container">dj</p>
+        <p onClick={aws} className="icon-container"><i id="i" class="fa-brands fa-aws"></i></p>
+        <p onClick={stripe} className="icon-container"><i id="i" class="fa-brands fa-stripe"></i></p>
+        <p onClick={node} className="icon-container"><i id="i" class="fa-brands fa-node-js"></i></p>
+        <p onClick={databases} className="icon-container">DB</p>
+        <p onClick={react}className="icon-container"><i id="i" class="fa-brands fa-react"></i></p>
+        <p onClick={github} className="icon-container"><i id="i" class="fa-brands fa-github"></i></p>
+        <p onClick={mongoose} className="icon-container">MG</p>
+        <p onClick={google} className="icon-container"><i id="i" class="fa-brands fa-google"></i></p>
+        <p onClick={vsCode} className="icon-container">VS</p>
+        <p onClick={postman} className="icon-container"><i id="i" class="fa-solid fa-person"></i></p>
       </div>
       </div>
 </div>
